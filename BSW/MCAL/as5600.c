5d14d8f476211b765ddd9b2b8b99d543bb2c67d2
Mai Duc Nam <namm.20004@gmail.com>
2026-02-04 20:17:18 +0700

    feat(sensorAS5600): Data collection has been implemented but parameters have not yet been adjusted.

--- BSW/MCAL/as5600.c
+++ BSW/MCAL/as5600.c
@@ -1,2 +1,29 @@
 #include "as5600.h"
 
+AS5600_Status_t AS5600_ReadAngle(I2C_HandleTypeDef *hi2c, uint16_t *pAngle)
+{
+    HAL_StatusTypeDef status;
+
+    uint8_t data[2] ={0,0};
+
+    if (pAngle == NULL) 
+    {
+        return AS5600_ERROR;
+    }
+
+    status = HAL_I2C_Mem_Read(hi2c,
+                            AS5600_ADDR,
+                            AS5600_ANGLE_HIGH,
+                            AS5600_ADDR_SIZE_8bit,
+                            data,
+                            2,
+                            100
+                              );
+    if (status != HAL_OK)
+    {
+        return AS5600_ERROR;
+    }
+
+    *pAngle = (((uint16_t)(uint16_t)data[0] << 8U) | (uint16_t)data[1]) & 0x0FFFU;
+    return AS5600_OK;
+}
--- BSW/MCAL/as5600.h
+++ BSW/MCAL/as5600.h
@@ -1,6 +1,21 @@
 #ifndef AS5600_H
 #define AS5600_H
 #include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_def.h"
+#include "stm32f4xx_hal_i2c.h"
+#include <stdint.h>
+#include <stdio.h>
 
+#define AS5600_ADDR (uint16_t)(0x36U <<1)
+#define AS5600_ANGLE_HIGH (uint16_t)(0x0EU)
+#define AS5600_ANGLE_LOW (uint16_t)(0x0FU)
+#define AS5600_ADDR_SIZE_8bit (0x00000001U)
+
+
+typedef enum {
+    AS5600_OK = 0,
+    AS5600_ERROR,
+    AS5600_TIMEOUT
+}AS5600_Status_t;
 
 #endif

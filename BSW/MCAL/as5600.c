9066288c0822f0e021035cf9d7453de99a4f70c6
Mai Duc Nam <namm.20004@gmail.com>
2026-02-04 20:52:54 +0700

    feat(sensorAS5600): Check the magnet position status

--- BSW/MCAL/as5600.c
+++ BSW/MCAL/as5600.c
@@ -1,4 +1,7 @@
 #include "as5600.h"
+#include "stm32f4xx_hal_i2c.h"
+#include <stdint.h>
+#include <stdio.h>
 
 AS5600_Status_t AS5600_ReadAngle(I2C_HandleTypeDef *hi2c, uint16_t *pAngle)
 {
@@ -25,5 +28,26 @@ AS5600_Status_t AS5600_ReadAngle(I2C_HandleTypeDef *hi2c, uint16_t *pAngle)
     }
 
     *pAngle = (((uint16_t)(uint16_t)data[0] << 8U) | (uint16_t)data[1]) & 0x0FFFU;
+    return AS5600_OK;
+}
+
+AS5600_Status_t AS5600_ReadStatus(I2C_HandleTypeDef *hi2c, uint8_t *pStatus)
+{
+    uint8_t status_reg = 0;
+    if (HAL_I2C_Mem_Read(hi2c, AS5600_ADDR, AS5600_STATUS, AS5600_ADDR_SIZE_8bit, &status_reg,1,100) ! HAL_OK)
+    {
+        return AS5600_ERROR;
+    }
+
+    if(pStatus != NULL)
+    {
+        *pStatus = status_reg;
+    }
+
+    if (!(status_reg & AS5600_STATUS_MD))
+    {
+        return AS5600_ERROR;
+    }
+
     return AS5600_OK;
 }
--- BSW/MCAL/as5600.h
+++ BSW/MCAL/as5600.h
@@ -10,12 +10,16 @@
 #define AS5600_ANGLE_HIGH (uint16_t)(0x0EU)
 #define AS5600_ANGLE_LOW (uint16_t)(0x0FU)
 #define AS5600_ADDR_SIZE_8bit (0x00000001U)
-
-
+#define AS5600_STATUS (uint16_t)(0x0BU)
+#define AS5600_STATUS_MD (uint8_t)(0x20U)
+#define AS5600_STATUS_ML (uint8_t)(0x10U)
+#define AS5600_STATUS_MH (uint8_t)(0x08U)
 typedef enum {
     AS5600_OK = 0,
     AS5600_ERROR,
     AS5600_TIMEOUT
 }AS5600_Status_t;
 
+AS5600_Status_t AS5600_ReadAngle(I2C_HandleTypeDef *hi2c, uint16_t *pAngle);
+AS5600_Status_t AS5600_ReadStatus(I2C_HandleTypeDef *hi2c, uint8_t *pStatus);
 #endif

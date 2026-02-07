602cd4a66032de33cfe6b80bdae55af94246ea7c
Mai Duc Nam <namm.20004@gmail.com>
2026-02-08 02:23:24 +0700

    chore: update cubeMX

--- STM32CubeMX/STM32F407VGTx/MX_Device/MX_Device.h
+++ STM32CubeMX/STM32F407VGTx/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 07/02/2026 23:15:34
+ * Date        : 08/02/2026 02:22:30
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
